d002228842d7ff80d4a462c1332ff64d1a7b04e8
Vinogradov Daniil <xitrix@bk.ru>
2018-12-04 15:09:34 +0300

    ViewModel for Contacts list view done

---
+++ src/Core/ViewModels/Contacts/Items/ContactItemVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Phonebook.API.Models;
+using Phonebook.API;
+namespace Phonebook.Core.ViewModels.Contacts.Items
+{
+    public class ContactItemVm
+    {
+        public User model;
+        public ContactItemVm(User model)
+        {
+            this.model = model;
+        }
+
+        public string FullName => $"{model.Name.last.FirstCharToUpper()} {model.Name.first.FirstCharToUpper()}";
+        public string PhotoPath => model.Picture.large;
+    }
+}
