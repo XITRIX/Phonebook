d85a0f35b5aaf5ac5dcf83390e086302de784d47
Vinogradov Daniil <xitrix@bk.ru>
2018-12-05 15:46:26 +0300

    Android contacts list activity done

---
+++ src/Droid/Views/Contacts/ContactsActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using Phonebook.Core.ViewModels.Contacts;
+
+namespace Phonebook.Droid.Contacts
+{
+    [MvxActivityPresentation]
+    [Activity(Theme = "@style/MainTheme")]
+    public class ContactsActivity : MvxAppCompatActivity<ContactsViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.activity_contacts_list);
+
+            var recyclerView = FindViewById<MvxRecyclerView>(Resource.Id.recyclerView);
+            var recyclerAdapter = new ContactsAdapter((IMvxAndroidBindingContext)BindingContext);
+
+            var set = this.CreateBindingSet<ContactsActivity, ContactsViewModel>();
+            set.Bind(recyclerAdapter).For(s => s.PagingCommand).To(vm => vm.LoadContactsCommand);
+            set.Bind(recyclerAdapter).For(s => s.IsLoading).To(vm => vm.IsLoading);
+            set.Apply();
+
+            recyclerView.Adapter = recyclerAdapter;
+        }
+    }
+}
+
