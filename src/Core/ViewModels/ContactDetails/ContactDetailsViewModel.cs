bdfd4c17795d4222b95b14c52aad7ac7ac9eefef
Vinogradov Daniil <xitrix@bk.ru>
2018-12-06 16:37:02 +0300

    ContactPhoto ViewModel added

--- src/Core/ViewModels/ContactDetails/ContactDetailsViewModel.cs
+++ src/Core/ViewModels/ContactDetails/ContactDetailsViewModel.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 using Phonebook.API.Models;
+using Phonebook.Core.ViewModels.ContactPhoto;
 using Phonebook.Core.ViewModels.Contacts.Items;
 namespace Phonebook.Core.ViewModels.ContactDetails
 {
     public class ContactDetailsViewModel : MvxViewModel<ContactItemVm>
     {
+        private readonly IMvxNavigationService _navigationService;
         private ContactItemVm _contact;
+
+        private IMvxAsyncCommand _navigateToPhotoCommand;
+        public IMvxAsyncCommand NavigateToPhotoCommand => _navigateToPhotoCommand ?? (_navigateToPhotoCommand = new MvxAsyncCommand(() => { return _navigationService.Navigate<ContactPhotoViewModel, string>(Photo); }));
 
         public string Photo => _contact.PhotoPath;
         public string Name => _contact.FullName;
         public string Phone => _contact.Phone;
         public string Mail => _contact.Mail;
+
+        public ContactDetailsViewModel(IMvxNavigationService navigationService) {
+            _navigationService = navigationService;
+        }
 
         public override void Prepare(ContactItemVm parameter)
         {
---
+++ src/Core/ViewModels/ContactPhoto/ContactPhotoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MvvmCross.ViewModels;
+namespace Phonebook.Core.ViewModels.ContactPhoto
+{
+    public class ContactPhotoViewModel : MvxViewModel<string>
+    {
+        public string Photo { get; set; }
+
+        public override void Prepare(string parameter)
+        {
+            Photo = parameter;
+        }
+    }
+}
