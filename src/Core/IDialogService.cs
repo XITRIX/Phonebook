b8b74e8539fa34661380d2cc44a3c845a3a2dd37
Vinogradov Daniil <xitrix@bk.ru>
2018-12-07 10:20:44 +0300

    Create IDialogService.cs

---
+++ src/Core/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Phonebook.Core
+{
+    public interface IDialogService
+    {
+        void CreateOneButtonDialog(string title, string message, string button, Action buttonAction);
+    }
+}
